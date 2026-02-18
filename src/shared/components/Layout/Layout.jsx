c06febf7a12ee6c5094a4e8e60a4707d563d3702
MaineMa <144504783+MaineMa@users.noreply.github.com>
2026-02-18 10:24:07 -0500

    feat: Add tailwindcss dependency

--- src/shared/components/Layout/Layout.jsx
+++ src/shared/components/Layout/Layout.jsx
@@ -3,11 +3,11 @@ import "./Layout.css";
 export default function Layout() {
     return (
         <>
-            <div className="layout">
-                <div className="sidebar">
+            <div className="flex flex-row min-h-screen min-w-screen">
+                <div className="basis-1/6">
                     <h1>Sidebar</h1>
                 </div>
-                <div className="maincontent">
+                <div className="basis-5/6">
                     <h1>Nav-bar</h1>
                     <h2>Content</h2>
                 </div>
