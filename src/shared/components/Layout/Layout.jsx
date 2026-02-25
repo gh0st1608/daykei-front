17172daac09821f7a79dfbb03197c74d7ec32dac
MaineMa <144504783+MaineMa@users.noreply.github.com>
2026-02-25 12:22:41 -0500

    feat: Add nav-bar component

--- src/shared/components/Layout/Layout.jsx
+++ src/shared/components/Layout/Layout.jsx
@@ -1,14 +1,15 @@
+import Navbar from "../Nav-bar/Nav-bar";
 import Sidebar from "../Side-bar/Sidebar";
 
 export default function Layout() {
     return (
         <>
-            <div className="flex flex-row bg-[#f4f5f4] h-screen">
-                <div className="basis-2/9 mx-3 my-1 p-3">
+            <div className="flex flex-row bg-[#f4f5f4] h-screen gap-2">
+                <div className="basis-2/9 my-1 p-3">
                     <Sidebar />
                 </div>
                 <div className="basis-7/9 p-3">
-                    <h1>Nav-bar</h1>
+                    <Navbar />
                     <h2>Content</h2>
                 </div>
             </div>
