9a60e4329c26427ffbb0705b0dcc7435c09e7d0a
MaineMa <144504783+MaineMa@users.noreply.github.com>
2026-03-02 18:44:02 -0500

    feat: Add content function component

---
+++ src/shared/components/Content/Content.jsx
@@ -0,0 +1,14 @@
+export default function Content() {
+    return (
+        <>
+            <div>
+                <section>
+                    
+                </section>
+                <section>
+
+                </section>
+            </div>
+        </>
+    );
+};
