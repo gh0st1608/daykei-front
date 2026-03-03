0499b6af0ac95ed314dec174d3f7ef7ea75cdbc7
MaineMa <144504783+MaineMa@users.noreply.github.com>
2026-03-03 11:26:58 -0500

    feat: Add content component to layout component. Add card html structure

--- src/shared/components/Content/Content.jsx
+++ src/shared/components/Content/Content.jsx
@@ -1,14 +1,85 @@
 export default function Content() {
-    return (
-        <>
-            <div>
-                <section>
-                    
-                </section>
-                <section>
-
-                </section>
+  return (
+    <>
+      <div className="flex flex-col gap-2 p-4">
+        <section className="shadow-xl rounded-xl flex flex-col py-5 bg-[#feffff]">
+          <div className="flex justify-between px-7 py-4">
+            <div className="flex flex-col gap-4 font-inter">
+              <p className="text-xs font-light">ID de Pago</p>
+              <h2 className="text-3xl font-bold">#123456</h2>
             </div>
-        </>
-    );
-};
+            <div className="flex flex-row items-end gap-5">
+              <div className="flex flex-row gap-3">
+                <img src="/Default_profile_image.png" className="w-6 h-6" />
+                <div className="flex flex-col font-inter">
+                  <p className="text-xs">Telefono</p>
+                  <h3 className="text-md font-bold text-[#56768b]">
+                    +51 987654321
+                  </h3>
+                </div>
+              </div>
+              <div className="flex flex-row gap-3">
+                <img src="/Default_profile_image.png" className="w-6 h-6" />
+                <div className="flex flex-col font-inter">
+                  <p className="text-xs">Correo</p>
+                  <h3 className="text-md font-bold text-[#56768b]">
+                    ejemplo@correo.com
+                  </h3>
+                </div>
+              </div>
+              <div className="flex flex-row gap-3">
+                <button className="rounded-xl cursor-pointer flex flex-row items-center gap-2 px-3 py-2 font-inter bg-[#f3f2f2] text-[#a8adb1]">
+                  <img src="/Default_profile_image.png" className="w-5 h-5" />
+                  Imprimir
+                </button>
+                <button className="rounded-xl cursor-pointer flex flex-row items-center gap-2 px-3 py-2 font-inter bg-[#59cec7] text-white">
+                  <img
+                    src="/Default_profile_image.png"
+                    className="w-5 h-5 invert"
+                  />
+                  Descargar reporte
+                </button>
+              </div>
+            </div>
+          </div>
+          <hr></hr>
+          <div className="px-4">
+            <div>
+              <img src="Default_image.png" className="w-20 h-20" />
+              <div>
+                <h2>Nombre y Apellido</h2>
+                <p>Red social</p>
+              </div>
+            </div>
+            <div>
+              <div>
+                <p>Metodo de pago</p>
+                <p>(Metodo de pago)</p>
+              </div>
+              <div>
+                <p>Fecha del recibo</p>
+                <p>(Fecha del recibo)</p>
+              </div>
+              <div>
+                <p>Fecha limite</p>
+                <p>(Fecha limite)</p>
+              </div>
+              <div>
+                <p>Fecha del pago</p>
+                <p>(Fecha del pago)</p>
+              </div>
+            </div>
+            <div>
+              <p>S/.</p>
+              <div>
+                <p>Cantidad</p>
+                <p>12345</p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section></section>
+      </div>
+    </>
+  );
+}
--- src/shared/components/Side-bar/Sidebar.jsx
+++ src/shared/components/Side-bar/Sidebar.jsx
@@ -1,52 +1,67 @@
 const menuItems = [
-    {
-        imageSrc: "/Default_profile_image.png",
-        name: "Departamentos"
-    },
-    {
-        imageSrc: "/Default_profile_image.png",
-        name: "Recibos"
-    },
-    {
-        imageSrc: "/Default_profile_image.png",
-        name: "Ajustes"
-    },
+  {
+    imageSrc: "/Default_profile_image.png",
+    name: "Departamentos",
+  },
+  {
+    imageSrc: "/Default_profile_image.png",
+    name: "Recibos",
+  },
+  {
+    imageSrc: "/Default_profile_image.png",
+    name: "Ajustes",
+  },
 ];
 
 export default function Sidebar() {
-    return (
-        <>
-            <div className="bg-[#fffeff] h-full p-5 box-border rounded-2xl flex flex-col items-center gap-6">
-                {/* Logo and Account */}
-                <div className="self-start w-full h-auto">
-                    <img src="/Default_image.png" alt="logo" className="box-border h-15 w-full" />
-                </div>
-                {/* Account */}
-                <div className="w-fit flex flex-row gap-4 items-center p-2 rounded-lg border-gray-300 border-2">
-                    <img src="/Default_profile_image.png" alt="profile" className="w-12 h-12 rounded-full" />
-                    <div>
-                        <p>Hola, <strong>Nombre</strong></p>
-                        <p>Correo</p>
-                    </div>
-                    <button>v</button>
-                </div>
-                {/* Navigation buttons */}
-                <div className="w-[80%] items-center">
-                    <ul className="flex flex-col gap-2">
-                        {menuItems.map((item, index) => (
-                            <li key={index} className="flex flex-row items-center gap-3 p-4 rounded-lg hover:bg-[#edf7f4] cursor-pointer transition-colors duration-150">
-                                <img src={item.imageSrc} alt={item.name} className="w-6 h-6" />
-                                <span>{item.name}</span>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-                {/* Copyright */}
-                <div className="grow flex flex-col justify-center">
-                    <strong className="text-s">Daykei Invoice Payment for Departments</strong>
-                    <p className="text-xs text-gray-500">Copyright © 2025</p>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="bg-[#fffeff] h-full p-5 box-border rounded-2xl flex flex-col items-center gap-6">
+        {/* Logo and Account */}
+        <div className="self-start w-full h-auto">
+          <img
+            src="/Default_image.png"
+            alt="logo"
+            className="box-border h-15 w-full"
+          />
+        </div>
+        {/* Account */}
+        <div className="w-fit flex flex-row gap-4 items-center p-2 rounded-lg border-gray-300 border-2">
+          <img
+            src="/Default_profile_image.png"
+            alt="profile"
+            className="w-12 h-12 rounded-full"
+          />
+          <div>
+            <p>
+              Hola, <strong>Nombre</strong>
+            </p>
+            <p>Correo</p>
+          </div>
+          <button>v</button>
+        </div>
+        {/* Navigation buttons */}
+        <div className="w-[80%] items-center">
+          <ul className="flex flex-col gap-2">
+            {menuItems.map((item, index) => (
+              <li
+                key={index}
+                className="flex flex-row items-center gap-3 p-4 rounded-lg hover:bg-[#edf7f4] cursor-pointer transition-colors duration-150"
+              >
+                <img src={item.imageSrc} alt={item.name} className="w-6 h-6" />
+                <span>{item.name}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+        {/* Copyright */}
+        <div className="grow flex flex-col justify-center">
+          <strong className="text-s">
+            Daykei Invoice Payment for Departments
+          </strong>
+          <p className="text-xs text-gray-500">Copyright © 2025</p>
+        </div>
+      </div>
+    </>
+  );
 }
